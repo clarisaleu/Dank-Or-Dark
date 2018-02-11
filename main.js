b97a9c4d8fef5a07184f255f9840da85ebb47c08
Clarisa Leu-Rodriguez <clarisarodriguezaz@gmail.com>
2018-02-10 17:37:54 -0800

    Update main.js

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ function randomNumberFromRange(min,max){
     return Math.floor(Math.random()*(max-min+1)+min);
 };
 
-
-var dankBtn = document.querySelector("#dank");    
+window.onload=function(){
+var dankBtn = document.getElementById("dank");    
 var darkBtn = document.querySelector("#dark");
-
 var dankMemeArray = ["https://www.reddit.com/r/dankmemes/.json", "https://www.reddit.com/r/Memes_Of_The_Dank/.json", "https://www.reddit.com/r/DNM_MEMES/.json"];
 var darkMemeArray = ["https://www.reddit.com/r/Dark_memes/.json", "https://www.reddit.com/r/OffensiveMemes/.json", "https://www.reddit.com/r/ImGoingToHellForThis/.json"];
 var randomDankMemeLink = randomNumberFromRange(0, dankMemeArray.length);
 var randomDarkMemeLink = randomNumberFromRange(0, darkMemeArray.length);
+};
 
 dankBtn.addEventListener("click",function loadImageDank(){
  		$.getJSON(memeArray[randomMemeDankLink], function (data) {
